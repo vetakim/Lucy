64065c74ced1ebbf7c1363ac480d8d57cf0e31fc
Ким Виолетта <vkim@rti-mints.ru>
2016-02-02 21:26:18 +0300

    Заполнение БД в представлении и кнопка для очистки БД

--- Lucy/ElfenLied/forms.py
+++ Lucy/ElfenLied/forms.py
@@ -4,4 +4,6 @@ from django import forms
 class CalcParams(forms.Form):
     amplitude = FloatField(label="Amplitude",
             widget=forms.TextInput(attrs={'class': 'amp', 'type': 'number'}))
+    toclear = BooleanField(required=False, widget=forms.TextInput(attrs={'class': 'cle',
+        'type': 'submit', 'value': 'Clear DB'}))
 
--- Lucy/ElfenLied/views.py
+++ Lucy/ElfenLied/views.py
@@ -2,17 +2,28 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from .models import *
 from .forms import *
+from numpy import random, sin, radians
 
 def accept_data(request):
     amplitude = 1.0
+    toclear = False
     if request.method == 'GET':
         form = CalcParams(request.GET)
         if form.is_valid():
             amplitude = form.cleaned_data['amplitude']
+            toclear = form.cleaned_data['toclear']
         else:
             print('FORM NOT VALID')
     else:
         form = CalcParams()
+
+    if toclear == True:
+        Point.objects.all().delete()
+
+    for j in range(0, 360, 20):
+        line = Point(abscissa=j, ordinate = amplitude * sin(radians(j)))
+        line.save()
+
     return render(request, 'lucyDJ/home.html',
             {"form": form,
                 "lines": Point.objects.all(),
