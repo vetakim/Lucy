6a3b3acc755a62808d813b6c9eff2e87aca66434
Ким Виолетта <vkim@rti-mints.ru>
2016-02-04 21:29:52 +0300

    Автоматическое заполнение БД вычисленными значениями

--- Lucy/ElfenLied/digen.py
+++ Lucy/ElfenLied/digen.py
@@ -1,5 +1,6 @@
 from numpy import radians, cos
 from numpy.random import uniform
+
 def gendi(amplitude, samples=360):
     n = amplitude / 10 ** (13 / 20)
     return [{
@@ -8,3 +9,10 @@ def gendi(amplitude, samples=360):
         'ordinate': uniform(-n, n) + amplitude * cos(radians(j))
         }
             for j in range(samples)]
+
+def entry_db(funcreturn, modelname):
+    '''Заполнение БД при помощи модели modelname списком словарей funcreturn'''
+    for dictionary in funcreturn:
+        modelname(**dictionary).save()
+    return 0
+
