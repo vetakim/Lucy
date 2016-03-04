f96efe0f4f75da81f6c8305d0fb33d4bb28c759d
Ким Виолетта <vkim@rti-mints.ru>
2016-03-04 15:52:41 +0300

    починила пай чарт

--- Lucy/ElfenLied/templatetags/calcfilters.py
+++ Lucy/ElfenLied/templatetags/calcfilters.py
@@ -1,4 +1,5 @@
 from django import template
+from collections import Counter
 
 register = template.Library()
 
@@ -12,5 +13,9 @@ def get(value, arg):
     '''getting value from dict'''
     return value.__getitem__(arg)
 
-# def categorized(list_, key):
-    
+@register.filter(name='categorized')
+def categorized(list_, key):
+    c = Counter()
+    for j in list_:
+        c[j[key]] += 1
+    return list(zip(c.keys(), c.values()))
